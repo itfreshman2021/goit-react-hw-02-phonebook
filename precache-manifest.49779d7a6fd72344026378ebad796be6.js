36706371a8f5a6873edf4067c5bfb96890a4b123
Ruslan Filonchuk <itfreshman@ukr.net>
2022-01-16 12:31:47 +0200

    Updates

---
+++ precache-manifest.49779d7a6fd72344026378ebad796be6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6045590aec51834b8373a1a61ac7dddf",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "e5ff8d3094696ea7274a",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7da278c41924b11db819",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.6371ef25.chunk.js"
+  },
+  {
+    "revision": "e5ff8d3094696ea7274a",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.942d82e3.chunk.js"
+  },
+  {
+    "revision": "259b50e1541406add527",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime~main.bf2c92ed.js"
+  }
+]);
