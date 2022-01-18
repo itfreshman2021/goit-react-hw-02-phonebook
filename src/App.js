30f873bca1e00a67548607e8de8d17639fea1dec
Ruslan Filonchuk <itfreshman@ukr.net>
2022-01-18 12:44:12 +0200

    added step 2 to the solution of task 2

--- src/App.js
+++ src/App.js
@@ -3,39 +3,27 @@ import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import s from './App.module.css';
 
-
 class App extends React.Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
   };
-
-  
-  contactNew = {
-      id: uuidv4(),
-      name: '',
-    };
 
   handleChange = event => {
     this.setState({ [event.currentTarget.name]: event.currentTarget.value });
-    this.contactNew = {
-      id: uuidv4(),
-      [event.currentTarget.name]: event.currentTarget.value,
-    };
-   
-
   };
 
   HandleSubmit = event => {
     event.preventDefault();
-    this.setState(({contacts}) => ({contacts: [this.contactNew,...contacts],}))
+
+    const contactNew = { id: uuidv4(), name: this.state.name, number: this.state.number };
+    this.setState(({ contacts }) => ({ contacts: [...contacts, contactNew] }));
     this.reset();
-  
-    console.log(this.state);
   };
 
   reset = () => {
-    this.setState({ name: '' });
+    this.setState({ name: '', number: '' });
   };
 
   render() {
@@ -57,17 +45,36 @@ class App extends React.Component {
                 className={s.input}
               />
             </label>
-            <button type="submit" className={s.button}>Add contact</button>
+            <label className={s.label}>
+              Number
+              <input
+                type="tel"
+                name="number"
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+                value={this.state.number}
+                onChange={this.handleChange}
+                required
+                className={s.input}
+              />
+            </label>
+
+            <button type="submit" className={s.button}>
+              Add contact
+            </button>
           </form>
           <h2>Contacts</h2>
           <ul>
             {this.state.contacts.map(contact => {
-              const { id, name } = contact;
+              const { id, name, number } = contact;
               return (
                 <li key={id} className={s.li}>
-                  <p>{name}</p>
-              </li>
-            );})} 
+                  <p>
+                    {name}:<span className={s.span}>{number}</span>
+                  </p>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </>
